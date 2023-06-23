1f2fb6fe1eb08f6e77b431376e1f9d5f0677d198
middle1 <rna050906@gmail.com>
2023-06-23 13:34:25 +0300

    Change name folder middle to middle1

---
+++ src/superarmor/middle1/libs/FormAPI/FormAPI.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace superarmor\middle1\libs\FormAPI;
+
+use pocketmine\plugin\PluginBase;
+
+class FormAPI extends PluginBase {
+	/**
+	 * @param callable|null $function
+	 * @return CustomForm
+	 * @deprecated
+	 *
+	 */
+	public function createCustomForm(?callable $function = NULL) : CustomForm {
+		return new CustomForm($function);
+	}
+
+	/**
+	 * @param callable|null $function
+	 * @return SimpleForm
+	 * @deprecated
+	 *
+	 */
+	public function createSimpleForm(?callable $function = NULL) : SimpleForm {
+		return new SimpleForm($function);
+	}
+
+	/**
+	 * @param callable|null $function
+	 * @return ModalForm
+	 * @deprecated
+	 *
+	 */
+	public function createModalForm(?callable $function = NULL) : ModalForm {
+		return new ModalForm($function);
+	}
+}
